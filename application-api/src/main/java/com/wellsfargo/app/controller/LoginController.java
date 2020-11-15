4b8934b72ae76b3c688bbc94c78b0e6f9e6f67b8
vishnuawasthi <vishnuawasthi121@gmail.com>
2020-11-15 21:25:57 +0530

    Logger message changes

--- application-api/src/main/java/com/wellsfargo/app/controller/LoginController.java
+++ application-api/src/main/java/com/wellsfargo/app/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.wellsfargo.app.controller;
 
 import com.wellsfargo.app.services.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,11 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api")
 public class LoginController {
 
+	private final Logger log   = LoggerFactory.getLogger(LoginController.class);
+
 	@Autowired
 	private LoginService loginService;
 
 	@PostMapping(path = "/v1/login", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Object doLogin() {
+		log.info("doLogin() - start");
+
+		log.info("doLogin() - end");
 		return new ResponseEntity<>(HttpStatus.OK);
 
 
